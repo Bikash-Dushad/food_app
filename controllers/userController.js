d6f4b2d0b14fa3bdef61725f9283852fd8c50752
Bikash-Dushad <bikashghll@gmail.com>
2024-06-09 17:16:29 +0530

    getting user data successfully

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,17 @@
+const User = require("../models/user")
+
 module.exports.getUserController =async (req, res)=>{
-    res.status(200).send("User Data");
+    //find user
+    try {
+        const user = await User.findById({_id: req.body.id})
+        if(!user){
+            return res.status(404).send({success: false, message: "User not found"})
+        }
+        //if user is founded, hide the password
+        user.password = undefined;
+        res.status(200).send({success: true, message: "User get successfully", user})
+    } catch (error) {
+        console.log(error);
+        res.status(500).send({success:false, message: "Error in user api", error});
+    }
 }
